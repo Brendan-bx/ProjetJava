db4b9741842213a5d8b84a86b85e2dd989eb5310
Brendan Bx <Brendanbx@gmail.com>
2025-02-13 11:20:52 +0100

    QuoteModel

---
+++ Projet_API/src/main/java/com/example/Projet_API/model/Quote.java
@@ -0,0 +1,40 @@
+package com.example.Projet_API.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Quote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String text;
+    private String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
